2aa1b3152d0ef381fae92917d1bbfa7b3092bbf1
Nic Pearson <nic.s.pearson@gmail.com>
2020-09-09 22:57:54 +0200

    chore: configure yarn2

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
+  plugins: [
+    ['@babel/plugin-transform-flow-strip-types'],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
 };
